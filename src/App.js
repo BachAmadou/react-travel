e165d46ecea5ce32e113e51a72f8ebbb5aff4074
Bachir <bachir@Bachirs-MacBook-Pro.local>
2022-07-06 00:40:26 -0400

    Added images to the projet, and content to the Navbar component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
 import Cards from "./Components/Cards"
+import Navbar from "./Components/Navbar";
 
 export default function App() {
   return(
     <div>
+      <Navbar />
       <Cards />
     </div>
   )
---
+++ src/Components/Navbar.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Navbar() {
+    return(
+        <div className="header">
+            <img src="../images/earth.png" alt="earth-glob" className="logo-icon" />
+            <h3>My travel journey</h3>
+        </div>
+    )
+}
