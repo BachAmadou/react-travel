ec0aba677288d65123ab7b7c675c983bdfd63491
Bachir <bachir@Bachirs-MacBook-Pro.local>
2022-07-12 00:41:30 -0400

    added content to the cards and modified some css and passed

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Navbar from "./Components/Navbar";
 import earth from "./images/earth.png";
 import fuji from "./images/fuji.jpg";
 import house from "./images/house.jpg";
+import water from "./images/water.jpg";
 
 export default function App() {
   return(
@@ -15,8 +16,30 @@ export default function App() {
         
       <Cards 
         imageName={fuji}
+        cardTitle="Japon"
+        cardMap="View on Google Maps"
+        cardH2="Mount Fuji"
+        cardDate="12 Jan, 2021 - 24 Jan, 2021"
+        cardText="Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists."
+      />
+
+      <Cards 
+        imageName={house}
+        cardTitle="AUSTRALIA"
+        cardMap="View on Google Maps"
+        cardH2="Sydney Opera House"
+        cardDate="27 May, 2021 - 8 Jun, 2021"
+        cardText="The Sydney Opera House is a multi-venue performing arts centre in Sydney. Located on the banks of the Sydney Harbour, it is often regarded as one of the 20th century's most famous and distinctive buildings."
       />
      
+     <Cards 
+        imageName={water}
+        cardTitle="NORWAY"
+        cardMap="View on Google Maps"
+        cardH2="Geirangerfjord"
+        cardDate="01 Oct, 2021 - 18 Nov, 2021"
+        cardText="The Geiranger Fjord is a fjord in the Sunnmøre region of Møre og Romsdal county, Norway. It is located entirely in the Stranda Municipality."
+      />
     </div>
   )
 }
--- src/Components/Cards.js
+++ src/Components/Cards.js
@@ -5,21 +5,30 @@ export default function Cards(props) {
         <div className="cards-wrapper">
             <div className="cards-item-1">
                 <img src={props.imageName} alt="mount-fuji" className="card-img" />
-                <span className="card-title">Japon</span>
-                <span className="card-map">View on Google Maps</span>
-                <h2 className="card-h2">Mount Fuji</h2>
-                <h2 className="card-date">12 Jan, 2021 - 24 Jan, 2021</h2>
-                <p className="card-text">Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.</p>
+                <span className="card-title">{props.cardTitle}</span>
+                <span className="card-map">{props.cardMap}</span>
+                <h2 className="card-h2">{props.cardH2}</h2>
+                <h2 className="card-date">{props.cardDate}</h2>
+                <p className="card-text">{props.cardText}</p>
             </div>
 
-            {/* <div className="cards-item-2">
+            <div className="cards-item-2">
                 <img src={props.imageName} alt="house" className="card-img" />
-                <span className="card-title">AUSTRALIA</span>
-                <span className="card-map">View on Google Maps</span>
-                <h2 className="card-h2">Sydney Opera House</h2>
-                <h2 className="card-date">27 May, 2021 - 8 Jun, 2021</h2>
-                <p className="card-text">The Sydney Opera House is a multi-venue performing arts centre in Sydney. Located on the banks of the Sydney Harbour, it is often regarded as one of the 20th century's most famous and distinctive buildings.</p>
-            </div> */}
+                <span className="card-title">{props.cardTitle}</span>
+                <span className="card-map">{props.cardMap}</span>
+                <h2 className="card-h2">{props.cardH2}</h2>
+                <h2 className="card-date">{props.cardDate}</h2>
+                <p className="card-text">{props.cardText}</p>
+            </div>
+
+            <div className="cards-item-3">
+                <img src={props.imageName} alt="water" className="card-img" />
+                <span className="card-title">{props.cardTitle}</span>
+                <span className="card-map">{props.cardMap}</span>
+                <h2 className="card-h2">{props.cardH2}</h2>
+                <h2 className="card-date">{props.cardDate}</h2>
+                <p className="card-text">{props.cardText}.</p>
+            </div>
         </div>
     )
 }
