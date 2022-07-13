fc77c7aaa8a304799922aa3b8cb4d2cf1a1db007
Bachir <bachir@Bachirs-MacBook-Pro.local>
2022-07-13 00:14:28 -0400

    added css style to card component, removed duplicated code

--- src/App.js
+++ src/App.js
@@ -12,8 +12,7 @@ export default function App() {
       <Navbar 
         imageName={earth}
         title="My travel journey"
-        />
-        
+        />      
       <Cards 
         imageName={fuji}
         cardTitle="Japon"
@@ -22,7 +21,6 @@ export default function App() {
         cardDate="12 Jan, 2021 - 24 Jan, 2021"
         cardText="Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists."
       />
-
       <Cards 
         imageName={house}
         cardTitle="AUSTRALIA"
@@ -30,8 +28,7 @@ export default function App() {
         cardH2="Sydney Opera House"
         cardDate="27 May, 2021 - 8 Jun, 2021"
         cardText="The Sydney Opera House is a multi-venue performing arts centre in Sydney. Located on the banks of the Sydney Harbour, it is often regarded as one of the 20th century's most famous and distinctive buildings."
-      />
-     
+      />    
      <Cards 
         imageName={water}
         cardTitle="NORWAY"
--- src/Components/Cards.js
+++ src/Components/Cards.js
@@ -3,31 +3,17 @@ import React from "react";
 export default function Cards(props) {
     return(
         <div className="cards-wrapper">
-            <div className="cards-item-1">
-                <img src={props.imageName} alt="mount-fuji" className="card-img" />
-                <span className="card-title">{props.cardTitle}</span>
-                <span className="card-map">{props.cardMap}</span>
-                <h2 className="card-h2">{props.cardH2}</h2>
-                <h2 className="card-date">{props.cardDate}</h2>
-                <p className="card-text">{props.cardText}</p>
-            </div>
-
-            <div className="cards-item-2">
-                <img src={props.imageName} alt="house" className="card-img" />
-                <span className="card-title">{props.cardTitle}</span>
-                <span className="card-map">{props.cardMap}</span>
-                <h2 className="card-h2">{props.cardH2}</h2>
-                <h2 className="card-date">{props.cardDate}</h2>
-                <p className="card-text">{props.cardText}</p>
-            </div>
-
-            <div className="cards-item-3">
-                <img src={props.imageName} alt="water" className="card-img" />
-                <span className="card-title">{props.cardTitle}</span>
-                <span className="card-map">{props.cardMap}</span>
-                <h2 className="card-h2">{props.cardH2}</h2>
-                <h2 className="card-date">{props.cardDate}</h2>
-                <p className="card-text">{props.cardText}.</p>
+            <div className="cards-items">
+                <div className="image">
+                    <img src={props.imageName} alt="mount-fuji" className="card-img" />
+                </div>
+                <div className="text">
+                    <span className="card-title">{props.cardTitle}</span>
+                    <span className="card-map">{props.cardMap}</span>
+                    <h2 className="card-h2">{props.cardH2}</h2>
+                    <h2 className="card-date">{props.cardDate}</h2>
+                    <p className="card-text">{props.cardText}</p>
+                </div>
             </div>
         </div>
     )
